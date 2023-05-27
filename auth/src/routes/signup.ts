14738c47d8dd7831a077ea090ffe8a95b00f1435
kunle002 <adekunle.olanipekun.ko@gmail.com>
2023-05-27 23:25:42 +0100

    signup endpoint

---
+++ auth/src/routes/signup.ts
@@ -0,0 +1,53 @@
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+import { User } from '../models/user';
+import { BadRequestError, validateRequest } from '@kunleticket/common'
+
+const router = express.Router();
+
+router.post('/api/users/signup', [
+  body("email").isEmail().withMessage('provide a valid email please'),
+  body('password').trim().isLength({ min: 8, max: 20 }).withMessage('provide a password'),
+  body('passwordConfirm').trim().isLength({ min: 8, max: 20 }).withMessage('please confirm your password')
+], validateRequest, async (req: Request, res: Response) => {
+
+  const { email, name, password, passwordConfirm } = req.body
+
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    throw new BadRequestError('this mail is already in use');
+  };
+
+  if (password !== passwordConfirm) {
+    throw new BadRequestError('please reconfirm your password');
+  };
+
+  const user = User.build({
+    email,
+    name,
+    password,
+    passwordConfirm
+  });
+
+  await user.save();
+
+  const userJwt = jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+      name: user.name
+    },
+    process.env.JWT_KEY!
+  );
+
+  req.session = {
+    jwt: userJwt,
+  };
+
+  res.status(201).send(user)
+});
+
+export { router as signupRouter }
+
+
