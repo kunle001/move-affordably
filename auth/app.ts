eb78002533048c6e9efdac74509fc2f7702fcee9
kunle002 <adekunle.olanipekun.ko@gmail.com>
2023-05-29 23:38:55 +0100

    added new routes

--- auth/app.ts
+++ auth/app.ts
@@ -2,8 +2,11 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 import { signupRouter } from './src/routes/signup';
-import { errorHandler, NotFoundError } from '@kunleticket/common'
+import { currentUser, errorHandler, NotFoundError } from '@kunleticket/common'
 import { signinRouter } from './src/routes/login';
+import { forgotPasswordRouter } from './src/routes/forgot-password';
+import { resetPasswordRouter } from './src/routes/reset-password';
+import { signoutRouter } from './src/routes/logout';
 
 
 const app = express();
@@ -11,10 +14,15 @@ const app = express();
 // middlewares
 app.use(cookieParser());
 app.use(bodyParser.json())
+app.use(currentUser);
+
 
 // routes
 app.use(signupRouter)
 app.use(signinRouter)
+app.use(forgotPasswordRouter)
+app.use(resetPasswordRouter)
+app.use(signoutRouter)
 
 // 
 app.use('*', () => {
