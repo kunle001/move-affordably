148e2442441c7c663b8cd419118e18d7aa597c9f
kunle002 <adekunle.olanipekun.ko@gmail.com>
2023-06-23 05:59:47 +0100

    delete image from file system once deleted from db

--- api/src/routes/apartment/delete.ts
+++ api/src/routes/apartment/delete.ts
@@ -1,6 +1,8 @@
 import { NotFoundError, currentUser, requireAuth } from '@kunleticket/common';
 import express from 'express';
 import { Apartment } from '../../models/apartment';
+import path from 'path';
+import fs from 'fs'
 
 const router = express.Router()
 
@@ -11,6 +13,15 @@ router.delete('/api/apartments/delete/:id',
     const apartment = await Apartment.findById(req.params.id);
 
     if (!apartment) throw new NotFoundError('no apartment with this id');
+
+    // Delete apartment images from directory '../../../../public/images'
+    apartment.images.forEach((image) => {
+      let imagePath = path.join(__dirname, '../../../../public/images', image)
+      console.log(imagePath)
+      if (fs.existsSync(imagePath)) {
+        fs.unlinkSync(imagePath)
+      }
+    })
 
     await apartment.deleteOne()
 
